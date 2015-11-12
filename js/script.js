98f3d8b1c53a130334fb93d95fcae577ea857a38
albertb22 <albertb22@hotmail.com>
2015-11-12 08:47:36 +0100

    Some changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
 	$('.button-collapse').sideNav({edge: 'left' });
 	$('.modal-trigger').leanModal();
+	$('select').material_select();
+	$('.datepicker').pickadate({
+    	selectMonths: true,
+    	selectYears: 25
+  	});
 });
